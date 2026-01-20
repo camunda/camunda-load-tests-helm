0cb622d3d40033c3eb6045df988db40b17f7c312
Christopher Zell <zelldon91@googlemail.com>
2026-01-20 14:40:48 +0100

    feat: introduce extraEnvs
     * To configure load test applications with environment variables we
       introduce new global value to set such on all deployments

---
+++ charts/camunda-load-tests/test/extra_env_test.go
@@ -0,0 +1,33 @@
+package test
+
+import (
+	"camunda-load-tests/charts/camunda-load-tests/test/golden"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestGoldenExtraEnvs(t *testing.T) {
+	// Test to validate that extra environments are correctly rendered in templates
+	chartPath, err := filepath.Abs("../")
+	require.NoError(t, err)
+	templateNames := []string{"workers", "starter"}
+
+	for _, name := range templateNames {
+		suite.Run(t, &golden.TemplateGoldenTest{
+			ChartPath:      chartPath,
+			Release:        "load-test",
+			Namespace:      "load-test-" + strings.ToLower(random.UniqueId()),
+			GoldenFileName: "golden-extra-envs-" + name,
+			Templates:      []string{"templates/" + name + ".yaml"},
+			SetValues: map[string]string{
+				"global.extraEnv[0].name":  "APP_MONITORDATAAVAILABILITY",
+				"global.extraEnv[0].value": "false",
+			},
+		})
+	}
+}
